244f9f96005326bdd021dedc99e2d1642f0c21b5
Abhishek <bhatiaabhishek776@gmail.com>
2025-11-23 23:43:11 +0530

    loader and useloaderData hook

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -7,17 +7,15 @@ export const Movie = () => {
     const movieData = useLoaderData();
     console.log(movieData);
     return (
-
-
-        <section className="movie-section">
+        <>
             <ul className="container grid grd-four--cols">
-                {getMoviesData.search.map((currMovie) => {
+                {movieData.Search.map((currMovie) => {
                     // whenevr you are using map method and calling a componenet then passing key is necessary
                     return <Card key={currMovie.imdbID} currMovie={currMovie} />;
                     // we are passing currMovie because we can easily destructure and gets it's properties 
                 })}
             </ul>
-        </section>
 
+        </>
     )
 }
