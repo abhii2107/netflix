6534427a37899343dd95edd42bc88687132838af
Abhishek <bhatiaabhishek776@gmail.com>
2025-11-24 00:06:20 +0530

    loading state using  usenavigation hook

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate, useNavigation } from "react-router-dom"
 import { Footer } from "./Footer"
 import { Header } from "./Header"
 
 const AppLayout = () => {
+    const navigation =useNavigation();
+    console.log(navigation)
+    if(navigation.state === "loading") return <h1>Loading...</h1>
     return (
         <>
             <Header />
