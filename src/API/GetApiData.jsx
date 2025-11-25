8919d305f474c97caa2e090e00b4590c9d92c964
Abhishek <bhatiaabhishek776@gmail.com>
2025-11-25 06:13:49 +0530

    using env to protect private data

--- src/API/GetApiData.jsx
+++ src/API/GetApiData.jsx
@@ -1,6 +1,6 @@
 export const getMoviesData = async()=>{
     try {
-       const response = await fetch("https://www.omdbapi.com/?s=guardians&apikey=526134df");
+       const response = await fetch(`https://www.omdbapi.com/?s=guardians&apikey=${import.meta.env.VITE_API_KEY}`);
        const data = await response.json();
        return data;
     } catch (error) {
